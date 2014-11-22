e7b2090484435c53a1e50225f162305aedd8358c
Crystal McQueen <camcqueen@bellsouth.net>
2014-11-21 22:28:24 -0500

    posting changes to R code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function computers the inverse of a square matrix and places it into the cached. If the function has already been run, the previous results will be retrieved without processing the function again.
 
-## Write a short comment describing this function
+## This creates a holding area for the results of the inverted matrix. Initially, the matrix is set to null or empty. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() m
+        list(set = set, get = get
+                    )
 }
 
 
-## Write a short comment describing this function
+## After the cacheSolve program is run once, the results will be stored in teh CacheMatrix (created above). This will allow the inverse to be called multiple times without requiring R to reprocess the solve function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+}
+cacheSolve <- function(x) {
+        m <- solve(x)
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        ## Searches if 'm' already has cached data. If yes, return 			stored data with a warning message "getting cached data"
+        }
+        data <- x$get()
+        m <- solve(data)
+        x$set(m)
+        m
+        ## If 'm' is null, run through the solve(x) function, return results, and store results to the cached matrix 'm'. If you rerun the cacheSolve again, the results will return without processing the function, and a warning of "getting cached data" will appear.
 }
